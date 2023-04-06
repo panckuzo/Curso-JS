ba43a7e82e209c04e7bd5ea86b4ae005c1e407f4
lucho <you@example.com>
2023-04-05 23:22:07 -0300

    se agrego boton wp

---
+++ js/array.js
@@ -0,0 +1,116 @@
+const ESPEJOS =
+    [
+    {producto: "ESPEJO JUANA MACRAME",
+    precio:4000
+    },
+    {producto:"ESPEJO ALONDRA MEDIANO", 
+    precio:6000},
+    {
+    producto:"ESPEJO SOL GRANDE",
+    precio:5000
+    },
+    ];
+
+    
+const LAMPARAS =
+    [
+    {producto:"LAMPARA ALMA",
+    precio: 9000
+    },
+    {producto:"LAMPARA LIBERTAD", 
+    precio:6000
+    }
+    ]
+
+const CANASTOS =
+    [
+    {producto: "CESTO DAMASCO GRANDE", 
+    precio: 4000},
+
+    {producto: "CESTO BRISA", 
+    precio:5000},
+
+    {producto: "CESTO LEÑERO RENATA", 
+    precio:7000},
+
+    {producto: "CESTO CALA", 
+    precio: 6000},
+
+    {producto: "CESTO BRASIL",
+    precio:6000},
+
+    {producto: "CAJON LAUREL MEDIANO", 
+    precio:7000}
+    ]; 
+
+const TAPICES =
+    [
+    {producto: "TAPICES MACRAME JOSE", 
+    precio: 4000},
+
+    {producto: "TAPIZ MACRAME TATI", 
+    precio: 4000},
+
+    {producto: "TAPIZ-NOE", 
+    precio: 8000},
+
+    {producto: "TAPIZ MACRAME PLUMA",
+    precio: 2500
+    }
+    ];
+
+const VELAS =
+    [
+    {producto: "FUSTA TRES VELAS AROMATICAS (Nag Champa)",
+    precio: $1500},
+
+    {producto: "FUSTA TRES VELAS AROMATICAS (Lavanda)",
+    precio: $1500}, 
+
+    {producto: "FUSTA TRES VELAS AROMATICAS (Jazmin)",
+    precio: $1500},
+
+    {producto: "VELA AROMATICA GRANDE (Nag Champa)",
+    precio: $1300},
+
+    {producto: "VELA AROMATICA GRANDE(Lavanda)",
+    precio: $1300},
+
+    {producto: "VELAS AROMATICA GRANDE (Jazmin)",
+    precio: $1300},
+
+    {producto: "VELA AROMATICA MEDIANA (Nag Champa)",
+    precio: $1100},
+
+    {producto: "VELA AROMATICA MEDIANA (Lavanda)",
+    precio: $1100},
+
+    {producto: "VELAS AROMATICA MEDIANA (Jazmin)",
+    precio:$1100}  
+    ];
+
+const PANERAS =
+    [
+    {producto: "PANAERA TRIGAL MEDIANA",
+    precio: 2100},
+
+    {producto: "PANERA TRIGAL GRANDE",
+    precio: 2800
+    }
+    ];
+
+const DECONAVIDAD =
+    [
+    {producto: "CORONA NAVIDEÑA", 
+    precio:$3700},
+
+    {producto: "ESTRELLAS", 
+    precio:$500 },
+
+    {producto: "ARBOLITO JERUSALEM GRANDE", 
+    precio: $3600},
+
+    {producto: "ARBOLITO JERUSALEM CHICO", 
+    precio: $3200},
+    ]; 
+
