4bed58b512f730cb6e463a536767dc47338407e0
lucho <you@example.com>
2023-05-16 21:48:37 -0300

    entrega final

--- js/nav-movil.js
+++ js/nav-movil.js
@@ -1,7 +1,7 @@
-// :: 2.2 Mobile Nav Active Code
-var amadoMobNav = document.querySelector('.amado-navbar-toggler');
-var navClose = document.querySelector('.nav-close');
-var headerArea = document.querySelector('.header-area');
+
+let amadoMobNav = document.querySelector('.amado-navbar-toggler');
+let navClose = document.querySelector('.nav-close');
+let headerArea = document.querySelector('.header-area');
 
 amadoMobNav.addEventListener('click', function () {
     headerArea.classList.toggle('bp-xs-on');
